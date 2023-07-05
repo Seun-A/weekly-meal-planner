2cbf5de2e73af54da96a8ce80b7c3e0ef6c6d11b
Seun-A <73300414+Seun-A@users.noreply.github.com>
2023-07-05 23:58:46 +0100

    First commmit and tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className={`
+          cookie-font text-5xl text-gray-900 py-2
+          sm:truncate sm:text-6xl sm:tracking-tight
+        `}>
+          Weekly Meal Plan
+        </div>
       </header>
     </div>
   );
