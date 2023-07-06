8f6268db7e387ed20e0a936a077376cd07d61e59
Seun-A <73300414+Seun-A@users.noreply.github.com>
2023-07-06 15:48:57 +0100

    UI first stage

---
+++ src/components/heading/heading.component.js
@@ -0,0 +1,10 @@
+const Heading = () => (
+  <div className={`
+      cookie-font text-5xl text-gray-900 py-2
+      sm:truncate sm:text-6xl sm:tracking-tight
+    `}>
+      Weekly Meal Plan
+  </div>
+)
+
+export default Heading;
---
+++ src/sections/header/header.section.js
@@ -0,0 +1,11 @@
+import Heading from "../../components/heading/heading.component";
+import DateSection from "../date/date.section";
+
+const Header = () => (
+  <header className="flex flex-col justify-center items-center">
+    <Heading />
+    <DateSection />
+  </header>
+)
+
+export default Header;
