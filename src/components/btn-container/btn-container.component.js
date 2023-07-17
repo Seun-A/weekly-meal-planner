a61527c6e5b1d544c708bdf4d31f0945bec81dc5
Seun-A <73300414+Seun-A@users.noreply.github.com>
2023-07-17 19:10:44 +0100

    Btn Component Reusable

--- src/components/btn-container/btn-container.component.js
+++ src/components/btn-container/btn-container.component.js
@@ -1,14 +1,9 @@
-const BtnContainer = ({ ...props }) => (
-  <div className={`btn-container absolute block w-48 top-60 flex justify-between`}>
-    <Btn btnStyle='submit' {...props} />
-    <Btn btnStyle='cancel' {...props} />
-  </div>
-)
+import Btn from "../btn/btn.component"
 
-const Btn = ({ form, setState, btnStyle, altState, showAltForm, toggleShowAltForm }) => {
+const BtnContainer = ({ form, setState, btnStyle, altState, showAltForm, toggleShowAltForm }) => {
   const cancel = event => {
     event.preventDefault()
-
+  
     if (form === "default") {
       setState({ day:'', meal:'', boxContent:'', alert:false })
     } else {
@@ -17,27 +12,13 @@ const Btn = ({ form, setState, btnStyle, altState, showAltForm, toggleShowAltFor
     }
   }
 
-  const [color, label, action] = btnStyle === "submit" ? 
-    [ 'bg-red-50', 'Submit', null ] : btnStyle === "cancel" ?
-    [ 'bg-red-300', 'Cancel', cancel ] :
-    [ 'bg-red-50', '?', null ]
 
   return (
-    <button className={`
-      ${color} py-2 shadow-md rounded-md text-sm font-semibold w-16
-
-      text-center cursor-pointer 
-
-      transition duration-200
-      hover:bg-red-400/50
-      focus:outline-none active:translate-y-1 active:shadow-none
-    `}
-      onClick={action}
-      type={btnStyle}
-    >
-      {label}
-    </button>
+    <div className={`btn-container absolute block w-48 top-60 flex justify-between`}>
+      <Btn btnStyle='submit' color='bg-red-50' label='Submit' onClick={null} />
+      <Btn btnStyle='cancel' color='bg-red-300' label='Cancel' onClick={cancel} />
+    </div>    
   )
-}
+}  
 
 export default BtnContainer;
